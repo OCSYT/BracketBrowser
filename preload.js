185194bc111e4c3eea9c6f9619e399ced39828cd
BracketProto <creepertruck812@gmail.com>
2023-12-13 21:42:49 +0000

    commit

---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+// Expose ipcRenderer to the renderer process
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: ipcRenderer,
+});
+
+
