c2e6986270757c51c85e0c3d82f977d5cc839349
BracketProto <creepertruck812@gmail.com>
2023-12-15 19:41:39 +0000

    plugins

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { app, BrowserWindow, ipcMain, screen, protocol, session  } = require('electron');
 const contextMenu = require('electron-context-menu');
 const path = require('path');
-
-
+const fs = require("fs");
+const process = require("process");
 
 contextMenu({
   showSaveImageAs: true
@@ -82,9 +82,37 @@ function createWindow() {
   return mainWindow;
 }
 
-function loadnewpage(url){
+function loadnewpage(mainWindow, url){
   mainWindow.webContents.executeJavaScript(`LoadNewLink(${url})`)
 }
+
+function loadPlugins(mainWindow) {
+  const pluginsFolderPath = path.join(path.dirname(process.resourcesPath), '/plugins/').replaceAll("\\", "/");
+  fs.readdir(pluginsFolderPath, (err, files) => {
+    if (err) {
+      console.log('Error reading plugins folder:', err);
+      return;
+    }
+
+    files.forEach(file => {
+      if (file.endsWith('.js')) {
+        const filePath = path.join(pluginsFolderPath, file);
+        fs.readFile(filePath, 'utf8', (error, data) => {
+          if (error) {
+            console.log(`Error reading file ${filePath}:`, error);
+            return;
+          }
+          // Execute the JavaScript code from the file in the mainWindow
+          mainWindow.webContents.executeJavaScript(data)
+            .catch(execError => {
+              console.log(`Error executing ${filePath}:`, execError);
+            });
+        });
+      }
+    });
+  });
+}
+
 
 protocol.registerSchemesAsPrivileged([
   { scheme: 'http', privileges: { standard: true, secure: true, supportFetchAPI: true } },
@@ -95,6 +123,8 @@ app.whenReady().then(() => {
 
   // Create the Electron window
   const mainWindow = createWindow();
+  loadPlugins(mainWindow, path.join(__dirname, '/plugins/'))
+
   app.on('open-url', (event, url) => {
     event.preventDefault();
     if (mainWindow) {
